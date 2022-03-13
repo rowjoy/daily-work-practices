f1b2ecc7ede7bef06fb86a3213e8ab0fc3199e6a
Jamirul islam (Joy) <69602585+rowjoy@users.noreply.github.com>
2022-03-13 10:39:53 +0600

    13-03-2022

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,19 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:imagetotext/String/string.dart';
 import 'package:imagetotext/model/login_model.dart';
 import 'package:imagetotext/service/api_service.dart';
+import 'package:imagetotext/views/user/home.dart';
 
 class LoginController extends GetxController {
   static final emailController = TextEditingController();
   static final passwordController = TextEditingController();
-  var isloading = true.obs;
   Future loginPost(Login login) async {
     ApiService.postdata(
       baseUri: Strings.loginBaseUrl,
       uri: Strings.loginUrl,
       data: login.toJson(),
-    );
-    isloading = false.obs;
+    ).then((data) {
+      if (data['message'] != null) {
+        Login _login = Login.fromJson(data);
+        if (_login.email!.isNotEmpty) {
+          Get.to(() => HomePage());
+        }
+      }
+    });
   }
 }
--- lib/controller/registertion_controller.dart
+++ lib/controller/registertion_controller.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:imagetotext/String/string.dart';
 import 'package:imagetotext/model/registration_model.dart';
 import 'package:imagetotext/service/api_service.dart';
 import 'package:get/get.dart';
+import 'package:imagetotext/views/user/home.dart';
 
 class RegistertionController extends GetxController {
   static final numberController = TextEditingController();
@@ -14,6 +17,14 @@ class RegistertionController extends GetxController {
       baseUri: Strings.registerBaseUrl,
       uri: Strings.registerUrl,
       data: registertion.toJson(),
-    );
+    ).then((value) {
+      if (value != null) {
+        Registertion _registertion = Registertion.fromJson(value);
+        // print('UI Id ${value['token']}');
+        if (_registertion.email!.isNotEmpty) {
+          Get.to(() => HomePage());
+        }
+      }
+    });
   }
 }
